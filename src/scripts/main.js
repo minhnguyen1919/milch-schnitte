064826ad7be54b2b4a888fe2693fad4f23942510
minhnguyen1919 <quangminhdn.vn@gmail.com>
2016-05-07 09:53:44 +0700

    Header, footer

---
+++ src/scripts/main.js
@@ -0,0 +1,5 @@
+// this is the main file that pulls in all other modules
+// you can require() bower components too!
+var example = require("./example");
+example.welcome();
+var $ = require("jquery");
