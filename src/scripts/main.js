379517d01255712144782e7f59b8df55986b4c3f
minhnguyen1919 <quangminhdn.vn@gmail.com>
2016-05-08 16:20:10 +0700

    handle selected for radio button item

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,20 @@
+
+
 $(document).ready(function() {
-  $(".summer-tabs-menu a").click(function(event) {
+
+  // for tabs
+  $('.summer-tabs-menu a').click(function (event) {
     event.preventDefault();
-    $(this).parent().addClass("active");
-    $(this).parent().siblings().removeClass("active");
-    var tab = $(this).attr("href");
-    $(".summer-tab-content").not(tab).css("display", "none");
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+    var tab = $(this).attr('href');
+    $('.summer-tab-content').not(tab).css('display', 'none');
     $(tab).fadeIn();
   });
+
+  // for answer radio buton
+  $('input[type=radio]').click(function () {
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+  })
 });
